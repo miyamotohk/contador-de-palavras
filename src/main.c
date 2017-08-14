71c3f145ea7ca1bce868fbb87626d748104f0233
Henrique Miyamoto <miyamotohk@gmail.com>
2017-08-14 00:20:50 -0300

    Realiza contagem de palavras formadas por letras e numeros simples (nao decimais)

--- src/main.c
+++ src/main.c
@@ -10,13 +10,48 @@
 int main() {
 
   char c;
+  int qtd = 0;
+  int flag = 0;
 
   c = 1;
 
   while (c != '\n') {
-    scanf("%c", &c);
+    scanf("%c", &c); //Realiza leitura de um caractere
+    
+    //Se o caractere for uma letra valida
+    if(((c >= 'a')&&(c <= 'z')) || ((c >= 'A')&&(c <= 'Z')))
+    {
+    	flag = 1; //Flag indica que o ultimo caractere foi uma letra
+    }
+    
+    //Se o caractere for um algarismo
+    else if((c>='0')&&(c<='9'))
+    {
+    	if(flag == 3)
+    		flag = 3;
+    	else
+			flag = 2; //Flag indica que o ultimo caractere foi um algarismo  
+    }
+    
+    //Se o caractere for outro simbolo
+    else
+    {
+    	if(flag == 1) //Se o ultimo foi uma letra, incrementa quantidade de palavras
+    	{
+    		qtd++;
+        flag = 0;
+    	}
+    	if(flag == 2) //Se foi um numero, incrementa a quantidade de palavras e altera a flag
+    	{
+    		//if((c == '.')||(c == ','))
+        qtd++;
+        flag = 0;
+    	}
+
+    }
+
   }
 
-  printf("1\n");
+  printf("%d\n", qtd);
   return 0;
 }
